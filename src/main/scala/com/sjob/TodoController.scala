dcaab9e79d816a8c542d0a2a0911b497f72d36b5
Steven Job <steven_job@live.com>
2016-05-04 20:35:22 +0100

    Add stage task for heroku and add deps for controller

--- src/main/scala/com/sjob/TodoController.scala
+++ src/main/scala/com/sjob/TodoController.scala
@@ -1,4 +1,10 @@
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.CrossOrigin
 
 @RestController
-@RequestMapping(value = "/todos")
+@CrossOrigin
+@RequestMapping(Array("/todos"))
 class TodoController {}
