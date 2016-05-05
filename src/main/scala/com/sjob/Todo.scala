a9e7d430251fd7c80d71d8a937a9df243c02a362
Steven Job <steven_job@live.com>
2016-05-05 20:26:17 +0100

    Add todo object and Create skeleton for rest endpoints

--- src/main/scala/com/sjob/Todo.scala
+++ src/main/scala/com/sjob/Todo.scala
@@ -4,5 +4,4 @@ class Todo {
   var id: Int = _
   var title: String = _
   var completed: Boolean = _
-  
 }
--- src/main/scala/com/sjob/TodoController.scala
+++ src/main/scala/com/sjob/TodoController.scala
@@ -1,14 +1,47 @@
 package com.sjob
 
 import org.springframework.web.bind.annotation._
+import org.springframework.validation.BindingResult
+import java.lang.Long
+import javax.validation.Valid
 
 @RestController
 @CrossOrigin
 @RequestMapping(Array("/todos"))
 class TodoController {
-  
-    @RequestMapping(method = Array(RequestMethod.GET))
-    def list() = {
+
+  @RequestMapping(method = Array(RequestMethod.GET))
+  def get() = {
+
+  }
+
+  @RequestMapping(value = Array("/{id}"), method = Array(RequestMethod.GET))
+  def getByID(@PathVariable("id") id: Long) = {
+
+  }
+
+  @RequestMapping(method = Array(RequestMethod.POST))
+  def create(@Valid todo: Todo, bindingResult: BindingResult) = {
+    if (bindingResult.hasErrors()) {
+      //TODO do something
+    } else {
 
     }
+  }
+
+  @RequestMapping(value = Array("/{id}"), method = Array(RequestMethod.PATCH))
+  def update(@PathVariable("id") id: String, @RequestBody todoUpdates: Todo) = {
+
+  }
+
+  @RequestMapping(value = Array("/{id}"), method = Array(RequestMethod.DELETE))
+  def deleteByID(@PathVariable("id") id: Long) = {
+
+  }
+
+  @RequestMapping(method = Array(RequestMethod.DELETE))
+  def deleteAll() {
+
+  }
+
 }
